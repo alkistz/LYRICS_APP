c72f0f009da0b787ac105015760439e4716d9b84
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2020-03-28 22:51:46 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+DatabaseCleaner.allow_production = true
+DatabaseCleaner.allow_remote_database_url = true
 DatabaseCleaner.clean_with(:truncation)
 
 
